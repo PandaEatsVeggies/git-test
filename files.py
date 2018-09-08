3f3c316e3cb5dc2420050ca0b035c72c31b95bfc
PandaEatsVeggies <39914313+PandaEatsVeggies@users.noreply.github.com>
2018-09-09 00:41:37 +0300

    Second commit

--- files.py
+++ files.py
@@ -1,4 +1,8 @@
-with open('config.ini', 'r') as rf:
-    with open('config_copy.ini', 'w') as wf:
-        for line in rf:
-            wf.write(line)
+with open('L7126.jpg', 'rb') as rf:
+    with open('L7126_copy.jpg', 'wb') as wf:
+        chunk_size = 4096
+        chunk = rf.read(chunk_size)
+        while len(chunk) > 0:
+            print(len(chunk))
+            wf.write(chunk)
+            chunk = rf.read(chunk_size)
